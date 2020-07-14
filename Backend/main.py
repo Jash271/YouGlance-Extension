21a98c63aca5024875f81ac354af89573c06d474
Jash271 <shahjash271@gmail.com>
2020-07-14 15:12:30 +0530

    cors update

--- Backend/main.py
+++ Backend/main.py
@@ -8,10 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-]
+origins = ['*']
 
 
 app.add_middleware(
